e6d2c388e6c3a036150802aef2bc7b9ca1e41257
Ayal Spitz <github@blackgecko.com>
2020-09-11 14:24:46 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ayal Spitz on 9/11/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
